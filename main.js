b10870591507fbdd6ca80a9dbeb115d4d33afda6
WincerChan <WincerChan@gmail.com>
2020-10-21 23:40:56 +0800

    add some dependence

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 import { registerRoute } from "workbox-routing";
-import { CacheFirst, StaleWhileRevalidate } from "workbox-strategies";
+import { CacheFirst, StaleWhileRevalidate, NetworkFirst } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
 
 precacheAndRoute(self.__WB_MANIFEST);
+console.log(self.__WB_MANIFEST)
 
 registerRoute(
     ({ request }) => request.destination === 'style',
     new StaleWhileRevalidate({
         cacheName: 'css-cache',
+    })
+)
+
+registerRoute(
+    ({ request }) => request.destination === "document",
+    new NetworkFirst({
+        cacheName: 'html-cache'
     })
 )
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,27 @@
 import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
 
 export default {
   input: 'main.js',
   output: {
-    file: 'sw.js',
-    format: 'iife'
+    file: 'service-worker.js',
+    format: 'cjs'
   },
   plugins: [
     resolve({
-        browser: true,
-      }),
+      browser: true,
+    }),
     commonjs(
     ),
     replace({
       __buildEnv__: 'production',
       __buildDate__: () => new Date(),
-      __buildVersion: 15
-    })
+      __buildVersion: 15,
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    terser()
   ],
 };
